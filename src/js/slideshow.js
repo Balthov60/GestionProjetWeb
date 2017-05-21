a5ffb52633af7f6dc91ad15b5507949575c85689
Balthov60 <Balthazarfrolin@gmail.com>
2017-05-21 16:05:22 +0200

    fix size problem for slideshow

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -2,11 +2,13 @@ var i = 1;
 
 function run() {
 	if (i == 1) {
-	  document.getElementById('restaurant_history_img').src = "images/historic_restaurant_1.jpg";
+	  document.getElementById('restaurant_history_img').style.backgroundImage = 'url(images/historic_restaurant_1.jpg)';
+	  document.getElementById('chief_history_img').style.backgroundImage = 'url(images/historic_chief_1.jpg)';
 	  i = 2;
 	}
 	else {
-	  document.getElementById('restaurant_history_img').src = "images/historic_restaurant_2.jpg";
+	  document.getElementById('restaurant_history_img').style.backgroundImage = 'url(images/historic_restaurant_2.jpg)';
+	  document.getElementById('chief_history_img').style.backgroundImage = 'url(images/historic_chief_2.jpg)';
 	  i = 1;
 	} 
 	setTimeout(run, 5000);
