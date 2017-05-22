a46918a71d89148f7030c65ad9ec1d1c81f2b5d3
Balthov60 <Balthazarfrolin@gmail.com>
2017-05-22 23:00:02 +0200

    Add Slideshow animation

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,17 +1,57 @@
-var i = 1;
+// Image Status
+var index = 1;
+var opacity = 0;
+
+// Images
+var images = document.getElementsByClassName('fade');
+var i;
+
+// Smooth Param
+var smoothDelay = 25;
+var smoothRate = 0.05;
 
 function run() {
-	if (i == 1) {
+	smooth();
+}
+
+function smooth() {
+	if (opacity < 1) {
+		opacity += smoothRate;
+		for (i = 0; i < images.length; i++) {
+		    images[i].style.opacity = opacity;
+		}
+		setTimeout(smooth, smoothDelay);
+	}
+	else {
+		changePicture();
+	}
+}
+
+function invertSmooth() {
+	if (opacity > 0) {
+		opacity -= smoothRate;
+		for (i = 0; i < images.length; i++) {
+		    images[i].style.opacity = opacity;
+		}
+		setTimeout(invertSmooth, smoothDelay);
+	}
+	else {
+		setTimeout(run, 5000);
+	}
+}
+
+function changePicture() {
+	if (index == 1) {
 	  document.getElementById('restaurant_history_img').style.backgroundImage = 'url(images/historic_restaurant_1.jpg)';
 	  document.getElementById('chief_history_img').style.backgroundImage = 'url(images/historic_chief_1.jpg)';
-	  i = 2;
+	  index = 2;
 	}
 	else {
 	  document.getElementById('restaurant_history_img').style.backgroundImage = 'url(images/historic_restaurant_2.jpg)';
 	  document.getElementById('chief_history_img').style.backgroundImage = 'url(images/historic_chief_2.jpg)';
-	  i = 1;
-	} 
-	setTimeout(run, 5000);
+	  index = 1;
+	}
+	setTimeout(invertSmooth, smoothDelay);
 }
 
 run();
