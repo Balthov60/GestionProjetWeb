d9b969c65dec87a403fa6b7857361765fdd63496
Balthov60 <Balthazarfrolin@gmail.com>
2017-05-23 17:14:00 +0200

    Add Scrolling animation

---
+++ src/js/smoothScrolling.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$('.middleScroll').click(function() {
+		var linkHref = $(this).attr('href');
+
+		var height = $(linkHref).offset().top - ("innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight) / 2;
+		if (height < 0)
+			height = 0;
+
+		$('html, body').animate( {
+			scrollTop: height
+		});
+
+		e.preventDefault();
+	});
+
+	$('.scroll').click(function() {
+		var linkHref = $(this).attr('href');
+
+		$('html, body').animate( {
+			scrollTop: $(linkHref).offset().top
+		});
+		
+		e.preventDefault();
+	});
+
+	$('.topScroll').click(function() {
+		$('html, body').animate( {
+			scrollTop: 1
+		});
+
+		e.preventDefault();
+	});
+});
