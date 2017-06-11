001bb3ea32eacf30f28b6e3961fd66958de9746f
Balthov60 <Balthazarfrolin@gmail.com>
2017-06-11 16:45:39 +0200

    Updatze mail

--- src/php/sendMail.php
+++ src/php/sendMail.php
@@ -5,7 +5,7 @@
 			 "\n"."Mail: ".$_GET['email'].
 			 "\n"."Téléphone: ".$_GET['tel'].
 			 "\n\n"."Commentaire: "."\n".$_GET['commentary'];
-	if(mail('Balthov60@gmail.com', 'Form Submission', $message, ''))
+	if(mail('lesadretsGPW@gmail.com', 'Form Submission', $message, ''))
 		echo 'Le mail à bien été envoyé, une réponse vous seras envoyé dans les plus bref délai.';
 	else 
 		echo 'Un problème à eu lieu avec le système de messagerie veuillez réessayer plus tard';
